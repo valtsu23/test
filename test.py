0fdae5a49362d0445992cd1c5ce3c1b0b9750669
valtsu23 <valtteri.eklund@gmail.com>
2023-09-26 13:27:20 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+import pygame
+import time
+
+#size = width, height = (1920, 1080)
+pygame.init()
+screen = pygame.display.set_mode(size)
+screen.fill((30, 30, 30))
+
+pygame.display.flip()
+time.sleep(5)
