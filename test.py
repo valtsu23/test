eb723e4dbb82f080fc8677bac712baf3072abdf3
valtsu23 <valtteri.eklund@gmail.com>
2023-09-26 13:27:40 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import pygame
 import time
 
-#size = width, height = (1920, 1080)
+size = width, height = (1920, 1080)
 pygame.init()
 screen = pygame.display.set_mode(size)
 screen.fill((30, 30, 30))
